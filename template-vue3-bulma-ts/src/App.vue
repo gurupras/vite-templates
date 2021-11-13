44f8a5bf5dde959a6fc98301d02b81565ea3ff69
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2021-11-14 05:00:22 +0530

    Added sources

---
+++ template-vue3-bulma-ts/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+<div>
+  <div class="container">
+    <div class="section">
+      <h1 class="title is-size-1"><i-mdi-check/> Ready</h1>
+    </div>
+  </div>
+</div>
+</template>
+
+<style>
+</style>
